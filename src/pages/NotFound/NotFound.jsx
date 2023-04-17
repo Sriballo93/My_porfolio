1b277c75fb8746db789ebb0ec3c2e6b74d03d19a
Sriballo93 <sebariba93@gmail.com>
2023-04-17 18:06:17 +0200

    ft changes

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -5,7 +5,7 @@ import './NotFound.css';
 import '../Home/Home.css';
 const NotFound = () => {
   return (
-    <main className="sr-main-home">
+    <main className="sr-main-notfound">
       <img
         className="sr-not"
         src="https://res.cloudinary.com/dg2xjawnt/image/upload/v1681745631/porfolio-sr/Dise%C3%B1o_sin_t%C3%ADtulo_1_v45hjd.gif"
