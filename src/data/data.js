546827c5be5d27573c2d89bd50b380a1de7ebd79
Sriballo93 <sebariba93@gmail.com>
2023-04-17 21:54:08 +0200

    ft changes

--- src/data/data.js
+++ src/data/data.js
@@ -3,8 +3,10 @@ export const works = [
     title: 'Activibe',
     video:
       'https://res.cloudinary.com/dg2xjawnt/video/upload/so_10/v1681579743/porfolio-sr/activibe_2_cevmzm.mp4',
+    description2:
+      'Activibe es una aplicación de turismo basada en MERN que permite a los usuarios buscar y reservar actividades en diferentes ciudades. Además, cuenta con una función de foro que permite a los usuarios interactuar y compartir información. El equipo de desarrollo de Sebastián Riballo, Jesús Román, Carlos García, Cristian Alonso y Jacobo Linares ha trabajado duro en el proyecto, creando una aplicación intuitiva, fácil de usar y accesible desde cualquier dispositivo. En general, Activibe es una aplicación útil y atractiva para los usuarios que deseen planificar actividades turísticas y conectarse con otros usuarios',
     description:
-      'Activibe es una aplicación de turismo basada en MERN que permite a los usuarios buscar y reservar actividades en diferentes ciudades. Además, cuenta con una función de foro que permite a los usuarios interactuar y compartir información. El equipo de desarrollo de Sebastián Riballo, Jesús Román, Carlos García, Cristian Alonso y Jacobo Linares ha trabajado duro en el proyecto, creando una aplicación intuitiva, fácil de usar y accesible desde cualquier dispositivo. En general, Activibe es una aplicación útil y atractiva para los usuarios que deseen planificar actividades turísticas y conectarse con otros usuarios.',
+      '.Activibe is a MERN-based tourism application that allows users to search and book activities in different cities. Additionally, it features a forum function that enables users to interact and share information. The development team consisting of Sebastián Riballo, Jesús Román, Carlos García, Cristian Alonso, and Jacobo Linares has worked hard on the project, creating an intuitive, user-friendly application accessible from any device. Overall, Activibe is a useful and attractive application for users looking to plan tourist activities and connect with others',
     technologies: 'HTML, CSS, JS, React Js, Vite, MongoDB, Express, NodeJs',
     year: '2023',
   },
@@ -13,8 +15,9 @@ export const works = [
     video:
       'https://res.cloudinary.com/dg2xjawnt/video/upload/so_10/v1681580214/porfolio-sr/cms_motor__1__bk2yfu.mp4',
     description:
+      'My first job in a team where Vite, React and MockAPI were used to create a CMS for motorcycle sales companies. The app allows businesses to create, read, edit, and delete their own ads, which will appear alongside other ads on the home page. By using components, we were able to reduce code and achieve more efficient development. I want to thank the team, Jesús Román, Laura Villarreal and Pablo Teijeiro, for their excellent work on this project.',
+    description2:
       'Mi primer trabajo en equipo donde se utilizó Vite, React y MockAPI para crear un CMS para empresas de venta de motos. La aplicación permite a las empresas crear, leer, editar y eliminar sus propios anuncios, que aparecerán junto con otros anuncios en el Home. Gracias al uso de componentes, pudimos reducir el código y lograr un desarrollo más eficiente. Quiero agradecer al equipo, Jesús Román, Laura Villarreal  y Pablo Teijeiro, por su gran trabajo en este proyecto.',
-    technologies: 'HTML, CSS, JS, React Js, Vite',
     year: '2023',
   },
   {
@@ -22,7 +25,9 @@ export const works = [
     video:
       'https://res.cloudinary.com/dg2xjawnt/video/upload/v1681573845/porfolio-sr/Harry_Potter_Fan_Page_-_Google_Chrome_2023-02-21_16-35-55_qfxo2x.mp4',
     description:
-      'Mi primer proyecto utilizando Vite en el que utilicé HTML, CSS y una pequeña cantidad de JavaScript. A pesar de ser mi primer proyecto, logré componerizar bastante para ahorrarnos el máximo código posible y aprendí mucho durante el proceso. Me siento agradecido por haber tomado la decisión de hacer el bootcamp en NEOLAND, ya que ha sido una experiencia enriquecedora que me ha permitido mejorar mis habilidades como desarrollador cada día.',
+      'My first project using Vite in which I used HTML, CSS and a small amount of JavaScript. Despite this being my first project, I managed to compose quite a bit to save as much code as possible, and I learned a lot in the process. I feel grateful for having made the decision to do the bootcamp at NEOLAND, as it has been an enriching experience that has allowed me to improve my skills as a developer every day.',
+    description2:
+      'Mi primer trabajo en equipo donde se utilizó Vite, React y MockAPI para crear un CMS para empresas de venta de motos. La aplicación permite a las empresas crear, leer, editar y eliminar sus propios anuncios, que aparecerán junto con otros anuncios en el Home. Gracias al uso de componentes, pudimos reducir el código y lograr un desarrollo más eficiente. Quiero agradecer al equipo, Jesús Román, Laura Villarreal  y Pablo Teijeiro, por su gran trabajo en este proyecto.',
     technologies: 'HTML, CSS, JS, Vite',
     year: '2022',
   },
